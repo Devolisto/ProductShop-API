7194e0420e78ff3b146bfd1020c606375af2c9d0
devolisto <devolisto@gmail.com>
2021-02-08 21:17:29 +0300

    CRUD done

--- app.js
+++ app.js
@@ -1,57 +1,122 @@
 // import products from "/products";
-let products = require("./products");
+// let products = require("./products");
 const express = require("express");
 const db = require("./db/models");
 const {Product} = require("./db/models");
 
+
 const app = express();
 app.use(express.json());
 
-app.get("/products/:productId", (req, res) => {
-    const foundProducts = products.find((product) => product.id === +req.params.productId);
+// app.get("/products/:productId", (req, res) => {
+//     const foundProducts = products.find((product) => product.id === +req.params.productId);
 
-    if(foundProducts) {
-        res.json(foundProducts);
-    } else {
-        res.status(404).json({message: "the product is not found"});
-    }
+//     if(foundProducts) {
+//         res.json(foundProducts);
+//     } else {
+//         res.status(404).json({message: "the product is not found"});
+//     }
     
-})
+// })
 
-app.post("/products", (req, res) => {
-    req.body.id = products[products.length -1].id + 1;
-    products.push(req.body);
-    res.status(201).json(req.body);
-})
-
-app.delete("/products/:productId", (req, res) => {
-    products = products.filter((product) => product.id !== +req.params.productId);
-    res.json({message: "the product has been deleted"}).status(204).end();
-    // if(products) {
-    //     res.status(204).json({message: "the product has been deleted"}).end();
-    // } else {
-    //     res.json({message: "the product has been deleted"}).end();    
-    // }
+// ALL PRODUCTS ROUTE
+app.get("/", async (req, res) => {
     
-})
-
-app.get("/products", (req, res) => {
-    res.json(products);
-})
-
-
-db.sequelize.sync();
-
-app.get("/productList", async (req, res) => {
     try {
       const products = await Product.findAll({
         attributes: { exclude: ["createdAt", "updatedAt"]}
       });
-      res.json(products);
+      res.status(200).json(products);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
   });
+
+// CREATE ROUTE
+app.post("/", async (req, res) => {
+
+    try {
+        const createProduct = await Product.create(req.body);
+        res.status(201).json(createProduct);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    // req.body.id = products[products.length -1].id + 1;
+    // products.push(req.body);
+    // res.status(201).json(req.body);
+})
+
+// PRODUCT ROUTE
+app.get("/:productId", async (req, res) => {
+
+    try {
+        const products = await Product.findByPk(req.params.productId);
+        if(products) {
+            res.status(200).json(products)
+        } else {
+             res.status(404).json({message: "Product is not found"});
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+// DELETE ROUTE
+app.delete("/:productId", async (req, res) => {
+
+    try {
+        const products = await Product.findByPk(req.params.productId);
+        if(products) {
+            await products.destroy();
+            res.status(204).end();
+        } else {
+             res.status(404).json({message: "Product is not found"});
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+
+// UPDATE ROUTE
+app.put("/:productId", async (req, res) => {
+    try {
+        const product = await Product.findByPk(req.params.productId);
+        if (product) {
+            await product.update(req.body);
+            res.status(204).json(product).end();
+        } else res.status(404).json({ message: "Product is not found" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+    });
+
+    // products = products.filter((product) => product.id !== +req.params.productId);
+    // res.json({message: "the product has been deleted"}).status(204).end();
+    // // if(products) {
+    // //     res.status(204).json({message: "the product has been deleted"}).end();
+    // // } else {
+    // //     res.json({message: "the product has been deleted"}).end();    
+    // // }
+    
+})
+
+// app.get("/products", (req, res) => {
+//     res.json(products);
+// })
+
+
+db.sequelize.sync({alter: true});
+// db.sequelize.sync({force: true});
+
+// app.get("/", async (req, res) => {
+//     try {
+//       const products = await Product.findAll({
+//         attributes: { exclude: ["createdAt", "updatedAt"]}
+//       });
+//       res.json(products);
+//     } catch (error) {
+//       res.status(500).json({ message: error.message });
+//     }
+//   });
 
 app.listen(8000, () => {
     console.log("hey thereeeeeeee")
--- db/models/Product.js
+++ db/models/Product.js
@@ -1,10 +1,19 @@
-const { Model, DataTypes } = require("sequelize");
-const { sequelize } = require(".");
+// const { Model, DataTypes } = require("sequelize");
+// const { sequelize, Sequelize } = require(".");
+const SequelizeSlugify = require("sequelize-slugify");
 
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define("Product", {
-        name: {type: DataTypes.STRING},
-        price: {type: DataTypes.STRING},
-        description: {type: DataTypes.STRING}
+    const Product = sequelize.define("Product", {
+        name: {type: DataTypes.STRING, allowNull: false,},
+        slug: {type: DataTypes.STRING, unique: true},
+        price: {type: DataTypes.INTEGER, defaultValue: 3, validate: { min: 2 }, allowNull: false,},
+        description: {type: DataTypes.STRING},
+        image: {type: DataTypes.STRING, allowNull: true},
     });
+
+    SequelizeSlugify.slugifyModel(Product, {
+        source: ["name"]
+      });
+
+    return Product;
 }
